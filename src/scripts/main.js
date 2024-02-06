0ab5a4ad188a3370393a5ea4e821f1910ca7b776
Kristiyan Karaychev <k.karaychev@gmail.com>
2024-02-06 18:20:42 +0200

    Added new page in folder 'pages,' and added functionality to 'Yes' button to redirect to a new page.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,19 @@
-document.getElementById('buttonYes').addEventListener('click', function() {
+document.getElementById("buttonYes").addEventListener("click", function () {
+    window.location.href = "/pages/yes.html";
+});
 
-})
-
-document.getElementById('buttonNo').addEventListener('click', function() {
-    
+document.getElementById("buttonNo").addEventListener("click", function () {
     // declaring a temporary variable to hold reusable code for clarity;
-    let buttonTemp = document.getElementById('buttonYes');  
-    
+    let buttonTemp = document.getElementById("buttonYes");
+
     // window.getComputedStyle(buttonTemp).fontSize returns the font size in "{num}px" format, so we use parseFloat to cut out the "px";
-    let buttonSizeTemp = parseFloat(window.getComputedStyle(buttonTemp).fontSize);
+    let buttonSizeTemp = parseFloat(
+        window.getComputedStyle(buttonTemp).fontSize
+    );
 
     // incrementing the font size by a given number
     buttonSizeTemp += 25;
 
     // changing the font size of the button to the new value; important to add the unit of measurement at the end (in our case "px");
-    document.getElementById('buttonYes').style.fontSize = buttonSizeTemp + 'px';
-    
-})
+    document.getElementById("buttonYes").style.fontSize = buttonSizeTemp + "px";
+});
