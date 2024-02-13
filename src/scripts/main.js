03f698f762fd4062b3b4a32ec01bf54bda4880f2
Kristiyan Karaychev <k.karaychev@gmail.com>
2024-02-13 13:32:12 +0200

    restructured main.js; fixed reset buttons on reload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttonYesTemp = document.getElementById("buttonYes");
-    let buttonNoTemp = document.getElementById("buttonNo");
-
-    buttonYesTemp.style.transform = "scale(1)";
-    buttonNoTemp.style.transform = "scale(1)";
-});
-
-document.getElementById("buttonYes").addEventListener("click", function () {
-    window.location.href = "pages/yes.html";
-});
-
 let statements_count = 0;
 let statement_repeating = "I will be sad";
 
-document.getElementById("buttonNo").addEventListener("click", function () {
-    // declaring a temporary variable to hold reusable code for clarity;
-    let buttonYesTemp = document.getElementById("buttonYes");
-    let buttonNoTemp = document.getElementById("buttonNo");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttonYes = document.getElementById("buttonYes");
+    let buttonNo = document.getElementById("buttonNo");
 
-    // window.getComputedStyle(buttonTemp).fontSize returns the font size in "{num}px" format, so we use parseFloat to cut out the "px";
-    let buttonSizeTemp = parseFloat(
-        window.getComputedStyle(buttonYesTemp).fontSize
-    );
+    buttonYes.addEventListener("click", function () {
+        window.location.href = "pages/yes.html";
+    });
 
-    // incrementing the font size by a given number
-    buttonSizeTemp += 25;
+    buttonNo.addEventListener("click", function () {
+        // declaring a temporary variable to hold reusable code for clarity;
+        let buttonYesTemp = document.getElementById("buttonYes");
+        let buttonNoTemp = document.getElementById("buttonNo");
 
-    let statements = [
-        "Please?",
-        "Are you sure?",
-        "Are you really sure?",
-        "I will be sad",
-    ];
+        // window.getComputedStyle(buttonTemp).fontSize returns the font size in "{num}px" format, so we use parseFloat to cut out the "px";
+        let buttonSizeTemp = parseFloat(
+            window.getComputedStyle(buttonYesTemp).fontSize
+        );
 
-    if (statements_count < 4) {
-        buttonNoTemp.innerText = statements[statements_count];
-        statements_count++;
-    } else {
-        statement_repeating =
-            statement_repeating.slice(0, 10) +
-            "very " +
-            statement_repeating.slice(10);
-        buttonNoTemp.innerText = statement_repeating;
-    }
+        // incrementing the font size by a given number
+        buttonSizeTemp += 25;
 
-    // changing the font size of the button to the new value; important to add the unit of measurement at the end (in this case "px");
-    document.getElementById("buttonYes").style.fontSize = buttonSizeTemp + "px";
+        let statements = [
+            "Please?",
+            "Are you sure?",
+            "Are you really sure?",
+            "I will be sad",
+        ];
+
+        if (statements_count < 4) {
+            buttonNoTemp.innerText = statements[statements_count];
+            statements_count++;
+        } else {
+            statement_repeating =
+                statement_repeating.slice(0, 10) +
+                "very " +
+                statement_repeating.slice(10);
+            buttonNoTemp.innerText = statement_repeating;
+        }
+
+        // changing the font size of the button to the new value; important to add the unit of measurement at the end (in this case "px");
+        document.getElementById("buttonYes").style.fontSize =
+            buttonSizeTemp + "px";
+    });
+
+    // resets buttons on reloading
+    resetButtons();
 });
+
+function resetButtons() {
+    buttonYes.style.transform = "scale(1)";
+    buttonNo.style.transform = "scale(1)";
+
+    statements_count = 0;
+    statement_repeating = "I will be sad";
+}
